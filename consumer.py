291c6440ae0c0ceb9df137ff87459bf15f7e8b2b
jackding96 <jackding96@outlook.com>
2019-08-21 17:47:03 -0400

    got simple producers and consumers working

--- consumer.py
+++ consumer.py
@@ -1,2 +1,6 @@
 from kafka import KafkaConsumer
 
+consumer = KafkaConsumer( 'test-multi-broker-topic',
+                          bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'])
+for message in consumer:
+    print (message.value.decode("utf-8") )
--- producer.py
+++ producer.py
@@ -1,2 +1,12 @@
 from kafka import KafkaProducer
+import threading
+import sys
 
+producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'])
+
+def sendMessage(count, prod):
+  producer.send('test-multi-broker-topic', f"{prod}: Message {count}".encode("utf-8"))
+  print(f"{prod}: Message {count}")
+  threading.Timer(1.0, sendMessage, [count+1, prod]).start()
+
+sendMessage(1, sys.argv[1])
