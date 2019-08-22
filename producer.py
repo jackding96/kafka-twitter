1af017382f68817e1392769c3aff68ec5a533995
jackding96 <jackding96@outlook.com>
2019-08-22 19:11:37 -0400

    trying to figure out how to hide twitter API creds

--- producer.py
+++ producer.py
@@ -1,12 +1,35 @@
 from kafka import KafkaProducer
 import threading
 import sys
+import json
+import requests
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'])
+url = "https://stream.twitter.com/1.1/statuses/filter.json"
 
-def sendMessage(count, prod):
-  producer.send('test-multi-broker-topic', f"{prod}: Message {count}".encode("utf-8"))
-  print(f"{prod}: Message {count}")
-  threading.Timer(1.0, sendMessage, [count+1, prod]).start()
+querystring = {"track":"javascript"}
 
-sendMessage(1, sys.argv[1])
+headers = {
+    'Content-Type': "application/x-www-form-urlencoded",
+    'Authorization': "OAuth oauth_consumer_key=\"Ysen3uq4Inu2TvWHaXBQgZ2eP\",oauth_token=\"2959444329-ndNO2oeHLmAtLGCt7htjs1dMYJe7WN6QPVGlaVq\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1566507687\",oauth_nonce=\"VIqWuqwhFXk\",oauth_version=\"1.0\",oauth_signature=\"8oBZ58K%2FIsUP2NfkmEj9LK7H0f4%3D\"",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Host': "stream.twitter.com",
+    'Accept-Encoding': "gzip, deflate",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+
+response = requests.request("GET", url, stream=True, headers=headers, params=querystring)
+
+for line in response.iter_lines():
+  if line:
+    decoded = line.decode('utf-8')
+    print(decoded)
+
+# producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'])
+
+# def sendMessage(count, prod):
+#   producer.send('test-multi-broker-topic', f"{prod}: Message {count}".encode("utf-8")).add_callback(print(f"{prod}: Message {count}"))
+#   threading.Timer(1.0, sendMessage, [count+1, prod]).start()
+
+# sendMessage(1, sys.argv[1])
---
+++ twitter-producer.py
@@ -0,0 +1,31 @@
+from kafka import KafkaProducer
+import threading
+import sys
+import tweepy
+import os
+
+consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+
+print(os.environ.get("twitter_test2"), consumer_key)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'])
+
+class MyStreamListener(tweepy.StreamListener):
+
+  def on_status(self, status):
+    print(status.text)
+    producer.send('test-multi-broker-topic', status.text.encode('utf-8'))
+
+twitter_streamer = MyStreamListener()
+twitter_stream = tweepy.Stream(auth=api.auth, listener=twitter_streamer)
+twitter_stream.filter(track=["coffee"], is_async=True)
+
+
